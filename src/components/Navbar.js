c5ef9d1bedf7e4440fb1c50b50d9dda833880183
Tino <tinomuzambi@gmail.com>
2020-12-31 18:05:04 +0200

    Removes bars code.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,35 @@
-import React, { useState } from "react";
-import { VscThreeBars } from "react-icons/vsc";
+import React from "react";
 
 function Navbar({ root }) {
-	const [shown, setShown] = useState(false);
-
 	return (
 		<nav className="nav">
-			<div className={`ul-wrapper ${shown ? "active" : ""}`}>
-				<VscThreeBars className="bars" onClick={() => setShown(!shown)} />
-				<div className="links">
-					<ul className="list">
-						<li
-							className="link"
-							onClick={() => root.home.scrollIntoView({ behavior: "smooth" })}
-						>
-							Home
-						</li>
-						<li
-							className="link"
-							onClick={() => root.about.scrollIntoView({ behavior: "smooth" })}
-						>
-							About
-						</li>
-						<li
-							className="link"
-							onClick={() =>
-								root.services.scrollIntoView({ behavior: "smooth" })
-							}
-						>
-							Services
-						</li>
-						<li
-							className="link"
-							onClick={() =>
-								root.contact.scrollIntoView({ behavior: "smooth" })
-							}
-						>
-							Contact
-						</li>
-					</ul>
-				</div>
+			<div className="links">
+				<ul className="list">
+					<li
+						className="link"
+						onClick={() => root.home.scrollIntoView({ behavior: "smooth" })}
+					>
+						Home
+					</li>
+					<li
+						className="link"
+						onClick={() => root.about.scrollIntoView({ behavior: "smooth" })}
+					>
+						About
+					</li>
+					<li
+						className="link"
+						onClick={() => root.services.scrollIntoView({ behavior: "smooth" })}
+					>
+						Services
+					</li>
+					<li
+						className="link"
+						onClick={() => root.contact.scrollIntoView({ behavior: "smooth" })}
+					>
+						Contact
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
