5f51217c5f53cb7cde5fa89fb38cd7dfbfb5a331
Tino <tinomuzambi@gmail.com>
2020-12-31 14:46:33 +0200

    Adds hero image styling.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,6 +7,10 @@ function Hero() {
 				<h1 className="title">Danmas Consultancy</h1>
 				<p className="subtitle">Helping businesses grow their finances.</p>
 			</div>
+			<div className="hero-img">
+				<img src="/assets/laptop.jpg" alt="Laptop" className="image" />
+				<p className="text">Welcome to our site</p>
+			</div>
 		</div>
 	);
 }
