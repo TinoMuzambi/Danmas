4c8555cc86fa006d1cefb7a273ff89e307871341
Tino <tinomuzambi@gmail.com>
2020-12-31 15:21:58 +0200

    Adds base contact form template.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Contact() {
-	return <div>Contact</div>;
+	const handleSubmit = () => {
+		console.log("Email!");
+	};
+
+	return (
+		<div className="contact">
+			<div className="form">
+				<form action={handleSubmit} className="contact-form">
+					<label htmlFor="name"></label>
+					<input type="text" name="name" id="name" />
+					<label htmlFor="email"></label>
+					<input type="email" name="email" id="email" />
+					<label htmlFor="message"></label>
+					<textarea name="message" id="message" cols="30" rows="10"></textarea>
+					<button type="submit">Contact us</button>
+				</form>
+			</div>
+			<div className="details"></div>
+		</div>
+	);
 }
 
 export default Contact;
