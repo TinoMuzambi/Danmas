486b3cd8c825c1c10c951066c7afe7f0760d61b5
Tino <tinomuzambi@gmail.com>
2020-12-31 15:30:42 +0200

    Adds address details.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 function Contact() {
 	const handleSubmit = () => {
@@ -9,16 +10,38 @@ function Contact() {
 		<div className="contact">
 			<div className="form">
 				<form action={handleSubmit} className="contact-form">
-					<label htmlFor="name"></label>
-					<input type="text" name="name" id="name" />
-					<label htmlFor="email"></label>
-					<input type="email" name="email" id="email" />
-					<label htmlFor="message"></label>
-					<textarea name="message" id="message" cols="30" rows="10"></textarea>
+					<label htmlFor="name">Name (required)</label>
+					<input type="text" name="name" id="name" required />
+					<label htmlFor="email">Email (required)</label>
+					<input type="email" name="email" id="email" required />
+					<label htmlFor="message">Message</label>
+					<textarea
+						name="message"
+						id="message"
+						cols="30"
+						rows="10"
+						required
+					></textarea>
 					<button type="submit">Contact us</button>
 				</form>
 			</div>
-			<div className="details"></div>
+			<div className="details">
+				<div className="socials">
+					<FaFacebook />
+					<FaLinkedin />
+					<FaTwitter />
+					<FaInstagram />
+				</div>
+				<a href="mailto:danmasara@gmail.com" className="email">
+					danmasara@gmail.com
+				</a>
+				<a href="tel:+277499889847">+277499889847</a>
+				<p className="text">25, 7th Avenue</p>
+				<p className="text">Belgravia</p>
+				<p className="text">Belgravia</p>
+				<p className="text">Cape Town, Western Cape 7764</p>
+				<p className="text">South Africa</p>
+			</div>
 		</div>
 	);
 }
