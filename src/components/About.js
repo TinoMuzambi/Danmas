a2a0db0e48ee52744b68b3a88d36bd340e9f6778
Tino <tinomuzambi@gmail.com>
2020-12-31 14:59:42 +0200

    Styles about section.

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 function About() {
-	return <div>About</div>;
+	return (
+		<div className="about">
+			<h1 className="title">Who We Are</h1>
+			<div className="list-wrapper">
+				<ul className="list">
+					<ul className="sublist">
+						<li className="item">
+							Our Vision
+							<ul className="list">
+								<li className="item">
+									To be a leader in assisting businesses attain economic growth.
+								</li>
+							</ul>
+						</li>
+						<li className="item">
+							Our Mission
+							<ul className="list">
+								<li className="item">
+									We assist businesses manage their financial resources by
+									combining research, technology and continuous improvement
+									through dedicated and competent employees.
+								</li>
+							</ul>
+						</li>
+						<li className="item">
+							Our Values
+							<ul className="list">
+								<li className="inner-item">Excellence</li>
+								<li className="inner-item">Integrity</li>
+								<li className="inner-item">Commitment</li>
+								<li className="inner-item">Confidentiality</li>
+							</ul>
+						</li>
+					</ul>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default About;
