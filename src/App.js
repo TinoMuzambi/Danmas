415ec8724120184533d83d76dd6e42e601b0876a
Tino <tinomuzambi@gmail.com>
2020-12-31 13:59:46 +0200

    Adds nav functionality.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./css/App.min.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -6,17 +7,44 @@ import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-function App() {
-	return (
-		<div>
-			<Navbar />
-			<Hero />
-			<About />
-			<Services />
-			<Contact />
-			<Footer />
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Navbar root={this} />
+				<section
+					ref={(section) => {
+						this.home = section;
+					}}
+				>
+					<Hero />
+				</section>
+				<section
+					ref={(section) => {
+						this.home = section;
+					}}
+				>
+					<About />
+				</section>
+				<section
+					ref={(section) => {
+						this.services = section;
+					}}
+				>
+					<Services />
+				</section>
+				<section
+					ref={(section) => {
+						this.contact = section;
+					}}
+				>
+					<Contact />
+				</section>
+
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { VscThreeBars } from "react-icons/vsc";
 
-function Navbar() {
-	return <div>Nav</div>;
+function Navbar({ root }) {
+	const [shown, setShown] = useState(false);
+
+	return (
+		<nav className="nav">
+			<div className={`ul-wrapper ${shown ? "active" : ""}`}>
+				<VscThreeBars className="bars" onClick={() => setShown(!shown)} />
+				<div className="links">
+					<ul className="list">
+						<li
+							className="link"
+							onClick={() => root.home.scrollIntoView({ behavior: "smooth" })}
+						>
+							Home
+						</li>
+						<li
+							className="link"
+							onClick={() => root.about.scrollIntoView({ behavior: "smooth" })}
+						>
+							About
+						</li>
+						<li
+							className="link"
+							onClick={() =>
+								root.services.scrollIntoView({ behavior: "smooth" })
+							}
+						>
+							Services
+						</li>
+						<li
+							className="link"
+							onClick={() =>
+								root.contact.scrollIntoView({ behavior: "smooth" })
+							}
+						>
+							Contact
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar;
