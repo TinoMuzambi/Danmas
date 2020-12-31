6b6a77c3ed8a0627e326b2a69258ba25702bee10
Tino <tinomuzambi@gmail.com>
2020-12-31 14:40:39 +0200

    Adds hero header styling.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Hero() {
-	return <div>Hero</div>;
+	return (
+		<div className="hero">
+			<div className="header">
+				<h1 className="title">Danmas Consultancy</h1>
+				<p className="subtitle">Helping businesses grow their finances.</p>
+			</div>
+		</div>
+	);
 }
 
 export default Hero;
