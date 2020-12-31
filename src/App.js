1621679cbfbb089620d11fa0e663cc63772664ea
Tino <tinomuzambi@gmail.com>
2020-12-31 13:45:29 +0200

    Adds components to App.js.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./css/App.min.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
-	return <div>hell</div>;
+	return (
+		<div>
+			<Navbar />
+			<Hero />
+			<About />
+			<Services />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
