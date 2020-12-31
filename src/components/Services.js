ac9fa6da2a8a44197c892b9d8b8d7a14285f55ee
Tino <tinomuzambi@gmail.com>
2020-12-31 15:16:14 +0200

    Adds services styling.

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { VscSymbolMisc, VscCreditCard } from "react-icons/vsc";
 
 function Services() {
-	return <div>Services</div>;
+	return (
+		<div className="services">
+			<h1 className="title">Services</h1>
+
+			<div className="service">
+				<VscCreditCard className="icon" />
+				<div className="content">
+					<h2 className="subtitle">Accounting and Bookkeeping Services</h2>
+					<ul className="list">
+						<li className="item">Payroll Preparation and Administration</li>
+						<li className="item">Taxation Advice</li>
+						<li className="item">Budgeting and Budgetary Control</li>
+						<li className="item">Purchasesd and Inventory Management</li>
+					</ul>
+				</div>
+			</div>
+
+			<div className="service">
+				<VscSymbolMisc className="icon" />
+				<div className="content">
+					<h2 className="subtitle">Other Services</h2>
+					<ul className="list">
+						<li className="item">
+							Company Registration and Corporate Governance
+						</li>
+						<li className="item">Investment Advice</li>
+						<li className="item">Entrepreneurship Training</li>
+						<li className="item">Risk Management and Business Continuity</li>
+						<li className="item">Strategy Formulation and Planning</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Services;
