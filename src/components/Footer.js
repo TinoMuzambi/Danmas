e58626efcd8757dbabaccbb9c3b8a5061b8abd70
Tino <tinomuzambi@gmail.com>
2021-02-04 22:13:43 +0200

    Replaces div with footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
 	return (
-		<div className="footer">
+		<footer className="footer">
 			<p className="text">Copyright 2021 © Danmas Consultancy</p>
-		</div>
+		</footer>
 	);
 }
 
